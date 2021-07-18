e469aebac514c5aa646267bdd0170e4d260f2899
Zehua Ren <renzholy@gmail.com>
2021-07-18 14:12:02 +0800

    Update getExplorerLink.ts

--- src/utils/getExplorerLink.ts
+++ src/utils/getExplorerLink.ts
@@ -1,16 +1,15 @@
 import { SupportedChainId } from '../constants/chains'
 
-const ETHERSCAN_PREFIXES: { [chainId: number]: string } = {
-  [SupportedChainId.MAINNET]: '',
-  [SupportedChainId.ROPSTEN]: 'ropsten.',
-  [SupportedChainId.RINKEBY]: 'rinkeby.',
-  [SupportedChainId.GOERLI]: 'goerli.',
-  [SupportedChainId.KOVAN]: 'kovan.',
+const STCSCAN_SUFFIXES: { [chainId: number]: string } = {
+  [SupportedChainId.MAINNET]: 'main',
+  [SupportedChainId.PROXIMA]: 'proxima',
+  [SupportedChainId.BARNARD]: 'barnard',
+  [SupportedChainId.HALLEY]: 'halley',
 }
 
 export enum ExplorerDataType {
   TRANSACTION = 'transaction',
-  TOKEN = 'token',
+  // TOKEN = 'token',
   ADDRESS = 'address',
   BLOCK = 'block',
 }
@@ -22,43 +21,43 @@ export enum ExplorerDataType {
  * @param type the type of the data
  */
 export function getExplorerLink(chainId: number, data: string, type: ExplorerDataType): string {
-  if (chainId === SupportedChainId.ARBITRUM_ONE) {
-    switch (type) {
-      case ExplorerDataType.TRANSACTION:
-        return `https://explorer.arbitrum.io/tx/${data}`
-      case ExplorerDataType.ADDRESS:
-        return `https://explorer.arbitrum.io/address/${data}`
-      case ExplorerDataType.BLOCK:
-        return `https://explorer.arbitrum.io/block/${data}`
-      default:
-        return `https://explorer.arbitrum.io/`
-    }
-  }
+  // if (chainId === SupportedChainId.ARBITRUM_ONE) {
+  //   switch (type) {
+  //     case ExplorerDataType.TRANSACTION:
+  //       return `https://explorer.arbitrum.io/tx/${data}`
+  //     case ExplorerDataType.ADDRESS:
+  //       return `https://explorer.arbitrum.io/address/${data}`
+  //     case ExplorerDataType.BLOCK:
+  //       return `https://explorer.arbitrum.io/block/${data}`
+  //     default:
+  //       return `https://explorer.arbitrum.io/`
+  //   }
+  // }
 
-  if (chainId === SupportedChainId.ARBITRUM_RINKEBY) {
-    switch (type) {
-      case ExplorerDataType.TRANSACTION:
-        return `https://rinkeby-explorer.arbitrum.io/tx/${data}`
-      case ExplorerDataType.ADDRESS:
-        return `https://rinkeby-explorer.arbitrum.io/address/${data}`
-      case ExplorerDataType.BLOCK:
-        return `https://rinkeby-explorer.arbitrum.io/block/${data}`
-      default:
-        return `https://rinkeby-explorer.arbitrum.io/`
-    }
-  }
+  // if (chainId === SupportedChainId.ARBITRUM_RINKEBY) {
+  //   switch (type) {
+  //     case ExplorerDataType.TRANSACTION:
+  //       return `https://rinkeby-explorer.arbitrum.io/tx/${data}`
+  //     case ExplorerDataType.ADDRESS:
+  //       return `https://rinkeby-explorer.arbitrum.io/address/${data}`
+  //     case ExplorerDataType.BLOCK:
+  //       return `https://rinkeby-explorer.arbitrum.io/block/${data}`
+  //     default:
+  //       return `https://rinkeby-explorer.arbitrum.io/`
+  //   }
+  // }
 
-  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] ?? ''}etherscan.io`
+  const prefix = `https://stcscan.io/${STCSCAN_SUFFIXES[chainId] ?? ''}`
 
   switch (type) {
     case ExplorerDataType.TRANSACTION:
-      return `${prefix}/tx/${data}`
+      return `${prefix}/transactions/detail/${data}`
 
-    case ExplorerDataType.TOKEN:
-      return `${prefix}/token/${data}`
+    // case ExplorerDataType.TOKEN:
+    //   return `${prefix}/token/${data}`
 
     case ExplorerDataType.BLOCK:
-      return `${prefix}/block/${data}`
+      return `${prefix}/blocks/detail/${data}`
 
     case ExplorerDataType.ADDRESS:
       return `${prefix}/address/${data}`
