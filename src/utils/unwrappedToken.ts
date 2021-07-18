43b7ff20c472fd0349868b8cba7b69889598d375
Zehua Ren <renzholy@gmail.com>
2021-07-18 16:54:52 +0800

    Update unwrappedToken.ts

--- src/utils/unwrappedToken.ts
+++ src/utils/unwrappedToken.ts
@@ -1,11 +1,11 @@
 import { Currency } from '@uniswap/sdk-core'
-import { ExtendedStar, WETH9_EXTENDED } from '../constants/tokens'
-import { supportedChainId } from './supportedChainId'
+// import { ExtendedStar, WETH9_EXTENDED } from '../constants/tokens'
+// import { supportedChainId } from './supportedChainId'
 
 export function unwrappedToken(currency: Currency): Currency {
-  if (currency.isNative) return currency
-  const formattedChainId = supportedChainId(currency.chainId)
-  if (formattedChainId && currency.equals(WETH9_EXTENDED[formattedChainId]))
-    return ExtendedStar.onChain(currency.chainId)
+  // if (currency.isNative) return currency
+  // const formattedChainId = supportedChainId(currency.chainId)
+  // if (formattedChainId && currency.equals(WETH9_EXTENDED[formattedChainId]))
+  //   return ExtendedStar.onChain(currency.chainId)
   return currency
 }
